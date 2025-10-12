0864729ed7d438fde0a39d2a36fd546031935bff
dreaming-panda <chenzhuoming911@gmail.com>
2025-10-12 05:05:43 -0400

    add installation

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install setuptools==78.1.1 pip==25.1
+pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1
+pip install flashinfer-python==0.2.7.post1 --no-deps --no-build-isolation
+pip install -e "python[all]"
--- python/sglang/srt/layers/attention/vtx_flashinfer_backend.py
+++ python/sglang/srt/layers/attention/vtx_flashinfer_backend.py
@@ -133,12 +133,12 @@ class VTXFlashInferAttnBackend(AttentionBackend):
         self.kv_indices = [
                 torch.zeros(
                     (
-                        (max_bs * self.num_kv_heads * model_runner.model_config.context_len + self.page_size - 1),), 
+                        (max_bs * self.num_kv_heads * model_runner.model_config.context_len + self.page_size - 1) // self.page_size,), 
                         dtype=torch.int32, device=model_runner.device
                 ),
                 torch.zeros(
                     (
-                        (max_bs * self.num_kv_heads * model_runner.model_config.context_len + self.page_size - 1),), 
+                        (max_bs * self.num_kv_heads * model_runner.model_config.context_len + self.page_size - 1) // self.page_size,), 
                         dtype=torch.int32, device=model_runner.device
                 ),
             ]
@@ -189,7 +189,6 @@ class VTXFlashInferAttnBackend(AttentionBackend):
                 )           
             ]
         
-         
         self.vtx_api = SparseAttentionServer(
                 head_dim=self.head_dim,
                 num_kv_heads=self.num_kv_heads,
@@ -222,8 +221,8 @@ class VTXFlashInferAttnBackend(AttentionBackend):
         if forward_batch.forward_mode.is_decode_or_idle():
             
             bs = len(forward_batch.req_pool_indices)
-            try:
-                self.vtx_api.plan_decode(
+            
+            self.vtx_api.plan_decode(
                     cached_seq_lens=forward_batch.seq_lens.to(torch.int32),
                     dense_kv_indptr=self.kv_indptr[1][:bs * self.num_kv_heads + 1],
                     dense_kv_indices=self.kv_indices[1],
@@ -232,21 +231,8 @@ class VTXFlashInferAttnBackend(AttentionBackend):
                     kv_last_page_len=self.kv_last_page_len[1][:bs * self.num_kv_heads],
                     req_to_token=self.req_to_token,
                     req_indices=forward_batch.req_pool_indices
-                )
-            except Exception as e:
-                print("Exception in plan_decode:")
-                print(f"bs: {bs}")
-                print(f"num_kv_heads: {self.num_kv_heads}")
-                print(f"dense_kv_indptr.shape: {self.kv_indptr[1].shape}")
-                print(f"sparse_kv_indptr.shape: {self.kv_indptr[0].shape}")
-                print(f"dense_kv_indptr used: {self.kv_indptr[1][:bs * self.num_kv_heads + 1].shape}")
-                print(f"sparse_kv_indptr used: {self.kv_indptr[0][:bs * self.num_kv_heads + 1].shape}")
-                print(f"kv_last_page_len.shape: {self.kv_last_page_len[1].shape}")
-                print(f"kv_last_page_len used: {self.kv_last_page_len[1][:bs * self.num_kv_heads].shape}")
-                print(f"req_to_token.shape: {self.req_to_token.shape}")
-                print(f"req_indices.shape: {forward_batch.req_pool_indices.shape}")
-                print(f"cached_seq_lens.shape: {forward_batch.seq_lens.shape}")
-                raise e
+            )
+            
             
             self.decode_wrappers[0].plan(
                 indptr=self.kv_indptr[0][:bs*self.num_kv_heads+1],
@@ -290,7 +276,7 @@ class VTXFlashInferAttnBackend(AttentionBackend):
                 req_to_token=self.req_to_token,
                 req_indices=forward_batch.req_pool_indices
             )
-
+   
             self.prefill_wrapper_ragged.plan(
                 self.qo_indptr[0][:bs+1],
                 self.qo_indptr[0][:bs+1],
@@ -308,11 +294,11 @@ class VTXFlashInferAttnBackend(AttentionBackend):
                 self.num_attn_groups,
                 1,
                 self.head_dim,
-                1,
+                self.page_size,
                 q_data_type=self.q_data_type,
                 kv_data_type=self.data_type,
                 custom_mask=None,
-                non_blocking=False,
+                non_blocking=True,
             )
             
 
@@ -399,10 +385,12 @@ class VTXFlashInferAttnBackend(AttentionBackend):
             )
             
             
-            
+            k_cache, v_cache = forward_batch.token_to_kv_pool.get_kv_buffer(layer.layer_id)
+            k_cache = k_cache.view(-1, self.page_size, 1, self.head_dim)
+            v_cache = v_cache.view(-1, self.page_size, 1, self.head_dim)
             o2, s2 = self.prefill_wrapper_paged.forward_return_lse(
                 q_t,
-                forward_batch.token_to_kv_pool.get_kv_buffer(layer.layer_id),
+                (k_cache, v_cache),
                 causal=False,
                 sm_scale=layer.scaling,
                 logits_soft_cap=logits_soft_cap,
@@ -447,7 +435,7 @@ class VTXFlashInferAttnBackend(AttentionBackend):
         v = v.view(-1, self.page_size, 1, self.head_dim)
         
         if use_sparsity:
-            #print(layer.layer_id)
+
             q_compress = q.contiguous().view(-1, self.num_attn_groups, layer.head_dim).sum(dim=-2)
             landmarks = forward_batch.token_to_kv_pool.get_landmark_buffer(layer.layer_id)
             
@@ -459,20 +447,8 @@ class VTXFlashInferAttnBackend(AttentionBackend):
                 sparse_kv_indptr=self.kv_indptr[0],
                 sparse_kv_indices=self.kv_indices[0]
             )
-            #torch.cuda.synchronize()
-            
             
             self.decode_wrappers[0]._paged_kv_indices_buf = self.kv_indices[0]
-            #torch.cuda.synchronize()
-            # torch.set_printoptions(profile="full")
-            # print(self.decode_wrappers[0]._paged_kv_indptr_buf)
-            # print(self.decode_wrappers[1]._paged_kv_indptr_buf)
-            # print(self.kv_indices[1][:190344].topk(10))
-            # # print(self.kv_indices[0][:27200])
-            # # print(self.kv_indices[0][:27200].max())
-            # print(self.kv_indices[0][:27200].topk(10))
-            # print(k.shape)
-            # print(v.shape)
             o = self.decode_wrappers[0].forward(
                 q.contiguous().view(-1, self.num_attn_groups, layer.head_dim),
                 (k, v),
@@ -482,16 +458,18 @@ class VTXFlashInferAttnBackend(AttentionBackend):
                 v_scale=layer.v_scale,
             )
             
-            #torch.cuda.synchronize()
-            # o = self.decode_wrappers[1].forward(
-            #     q.contiguous().view(-1, self.num_attn_groups, layer.head_dim),
-            #     (k, v),
-            #     sm_scale=layer.scaling,
-            #     logits_soft_cap=layer.logit_cap,
-            #     k_scale=layer.k_scale,
-            #     v_scale=layer.v_scale,
-            # )
         else:
+            # q_compress = q.contiguous().view(-1, self.num_attn_groups, layer.head_dim).sum(dim=-2)
+            # landmarks = forward_batch.token_to_kv_pool.get_landmark_buffer(layer.layer_id)
+            
+            # self.vtx_api.get_sparse_kv_indices(
+            #     query=q_compress,
+            #     landmarks=landmarks.view(-1, layer.head_dim),
+            #     dense_kv_indptr=self.kv_indptr[1],
+            #     dense_kv_indices=self.kv_indices[1],
+            #     sparse_kv_indptr=self.kv_indptr[0],
+            #     sparse_kv_indices=self.kv_indices[0]
+            # )
             o = self.decode_wrappers[1].forward(
                 q.contiguous().view(-1, self.num_attn_groups, layer.head_dim),
                 (k, v),
@@ -500,6 +478,14 @@ class VTXFlashInferAttnBackend(AttentionBackend):
                 k_scale=layer.k_scale,
                 v_scale=layer.v_scale,
             )
+            
+            # o = self.decode_wrappers[1].forward(
+            #     q.contiguous().view(-1, self.num_attn_groups, layer.head_dim),
+            #     (k, v),
+            #     sm_scale=layer.scaling,
+            #     logits_soft_cap=layer.logit_cap,
+            #     k_scale=layer.k_scale,
+            #     v_scale=layer.v_scale,
+            # )
 
         return o.view(-1, layer.tp_q_head_num * layer.head_dim)
-
