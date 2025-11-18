ae25de52d086f7d57bfe43df5e18b1324ee2dea2
dreaming-panda <chenzhuoming911@gmail.com>
2025-11-18 18:31:05 -0500

    examples update

--- tests/offline_gen.py
+++ tests/offline_gen.py
@@ -3,10 +3,11 @@ import sys
 sys.path.append("../")
 import python.sglang as sgl
 from transformers import AutoTokenizer
+import time
 def main():
-    model_name = "Qwen/Qwen3-1.7B"
+    model_name = "Qwen/Qwen3-0.6B"
     llm = sgl.Engine(model_path=model_name, 
-                    disable_cuda_graph=True,
+                    disable_cuda_graph=False,
                     page_size=16,
                     vortex_topk_val=30,   
                     disable_overlap_schedule=True,
@@ -17,7 +18,7 @@ def main():
                     vortex_layers_skip=list(range(1)),
                     vortex_module_name="block_sparse_attention",
                     vortex_max_seq_lens=8192,
-                    mem_fraction_static=0.7
+                    mem_fraction_static=0.9
                     )
     
     with open('story.txt', "r", encoding="utf-8") as file:
@@ -33,19 +34,33 @@ def main():
         text,
         tokenize=False,
         add_generation_prompt=True,
-        enable_thinking=False
+        enable_thinking=True
     ) for text in texts
     ]
     
-    prompts = prompts * 4
+    prompts = prompts * 128
+    sampling_params = {"temperature": 0.6, "top_p": 0.95, "top_k": 20, "max_new_tokens": 2048}
 
-    sampling_params = {"temperature": 0.6, "top_p": 0.95, "top_k": 20, "max_new_tokens": 4096}
+    total_tokens = 0
+    total_time = 0.0
+    e2e_time = 0
+
+    start = time.perf_counter()
+
+    o = llm.generate(prompts, sampling_params)
+
+    elapsed = time.perf_counter() - start
+    total_time += elapsed
+
     with open("output.jsonl", "w", encoding="utf-8") as f:
-        for _ in range(1):
-            o = llm.generate(prompts, sampling_params)
-            for item in o:
-                    json.dump(item, f, ensure_ascii=False)
-                    f.write("\n")
+        for item in o:
+            total_tokens += item["meta_info"]["completion_tokens"] 
+            e2e_time = max(e2e_time, item["meta_info"]["e2e_latency"])
+            json.dump(item, f, ensure_ascii=False)
+            f.write("\n")
+
+        meta_data = {"e2e_time": e2e_time, "total_time": total_time, "total_tokens": total_tokens, "throughput": total_tokens / total_time}
+        print(meta_data)
 
 if __name__ == "__main__":
     main()
