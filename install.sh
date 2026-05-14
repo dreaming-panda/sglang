d4d00546117b407cf9542b2c09d754f779e9268e
Zhuoming Chen <chenzhuoming911@gmail.com>
2026-05-13 22:31:28 -0400

    JIT Compilation

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-pip install setuptools==78.1.1 pip==25.1
 pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
 pip install flashinfer-python==0.6.8.post1
 pip install PyYAML==6.0.3
